c32e5b705c358980132d00b123b83431d020f661
SaiHome <sai@saisaga.com>
2022-09-13 20:39:49 +0700

    e-7

--- Assets/_Data/PhotonPlaying.cs
+++ Assets/_Data/PhotonPlaying.cs
@@ -7,11 +7,51 @@ using UnityEngine;
 public class PhotonPlaying : MonoBehaviourPunCallbacks
 {
     public static PhotonPlaying instance;
+    public string photonPlayerName = "PhotonPlayer";
+    public List<PlayerProfile> players = new List<PlayerProfile>();
 
     protected void Awake()
     {
         PhotonPlaying.instance = this;//Dont do this in your game
+
+        this.LoadRoomPlayers();
+        this.SpawnPlayer();
     }
+
+    protected virtual void SpawnPlayer()
+    {
+        if (PhotonNetwork.NetworkClientState == ClientState.Joined)
+        {
+            this.LoadPlayerPrefab();
+            return;
+        }
+
+        GameObject playerObj = Resources.Load(this.photonPlayerName) as GameObject;
+        Instantiate(playerObj);
+    }
+        
+
+    protected virtual void LoadRoomPlayers()
+    {
+        if (PhotonNetwork.NetworkClientState != ClientState.Joined) return;
+
+        PlayerProfile playerProfile;
+        foreach (KeyValuePair<int, Player> playerData in PhotonNetwork.CurrentRoom.Players)
+        {
+            Debug.Log(playerData.Value.NickName);
+            playerProfile = new PlayerProfile
+            {
+                nickName = playerData.Value.NickName
+            };
+            this.players.Add(playerProfile);
+        }
+    }
+
+    protected virtual void LoadPlayerPrefab()
+    {
+        PhotonNetwork.Instantiate(this.photonPlayerName,Vector3.zero,Quaternion.identity);
+    }
+
 
     public virtual void Leave()
     {
